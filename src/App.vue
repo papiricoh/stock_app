aa4ff0a4aa1c3111020fa0500756d7af34a8f859
Papiricoh <slendeman0200@gmail.com>
2023-05-10 15:28:28 +0200

    added tailwindCss

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,18 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
+  <div class="flex main_app">
+    <header class="xl:rounded-r transform  xl:translate-x-0  ease-in-out transition duration-500 flex justify-start items-start h-full  w-full sm:w-64 bg-gray-900 flex-col">
+      <div class="mt-6 flex flex-col justify-start items-center  pl-4 w-full border-gray-600 border-b space-y-3 pb-5 ">
+        <RouterLink class="flex jusitfy-start items-center space-x-6 w-full  focus:outline-none  focus:text-indigo-400  text-white rounded" to="/">
+          <div class="text-base leading-4">Dashboard</div>
+        </RouterLink>
+      </div>
+    </header>
     <div>
-      <RouterLink to="/">Home</RouterLink>
+      dwhudihwh
     </div>
-  </header>
-
-  <RouterView />
+    <RouterView />
+    
+  </div>
 </template>
